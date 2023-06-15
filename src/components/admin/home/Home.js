84a03b2f59f2e490314960bee78fb4dd03741605
keipy27 <kevinandris27@gmail.com>
2023-06-15 20:56:02 +1200

    earnings, products, orders containers added in admin home page

--- src/components/admin/home/Home.js
+++ src/components/admin/home/Home.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import InfoBox from "../../infoBox/InfoBox"
 import styles from "./Home.module.scss"
+import React from 'react'
+import { AiFillDollarCircle } from 'react-icons/ai'
+import { BsCart4 } from 'react-icons/bs'
+import { FaCartArrowDown } from 'react-icons/fa'
+
+// * ICONS
+const earningIcon = <AiFillDollarCircle size={30} color="#b624ff" />
+const productIcon = <BsCart4 size={30} color="#1f93ff" />
+const ordersIcon = <FaCartArrowDown size={30} color="orangered" />
 
 const home = () => {
   return (
-    <div>home</div>
+    <div className={styles.home}>
+      <h2>Admin Home</h2>
+      <div className={styles["info-box"]}>
+
+        {/* EARNINGS */}
+        <InfoBox  
+          cardClass={`${styles.card} ${styles.card1}`}
+          title={"Earnings"}
+          count={"188"}
+          icon={earningIcon}
+        />
+
+        {/* PRODUCTS */}
+        <InfoBox 
+          cardClass={`${styles.card} ${styles.card2}`}
+          title={"Products"}
+          count={"78"}
+          icon={productIcon}
+        />
+
+        {/* ORDERS */}
+        <InfoBox 
+          cardClass={`${styles.card} ${styles.card3}`}
+          title={"Orders"}
+          count={"50"}
+          icon={ordersIcon}
+        />
+
+      </div>
+    </div>
   )
 }
 
--- src/components/infoBox/InfoBox.js
+++ src/components/infoBox/InfoBox.js
@@ -1,9 +1,19 @@
+// ! 41
+import Card from '../card/Card'
 import styles from './InfoBox.module.scss'
 import React from 'react'
 
-const InfoBox = () => {
+const InfoBox = ({cardClass, title, count, icon}) => {
   return (
-    <div>InfoBox</div>
+    <div className={styles["info-box"]}>
+        <Card cardClass={cardClass}>
+            <h4>{title}</h4>
+            <span>
+                <h3>${count}</h3>
+                {icon}
+            </span>
+        </Card>
+    </div>
   )
 }
 
